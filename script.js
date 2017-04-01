16a84adaba15b496b3991965866aa540856b65a8
lujanfernaud <lujanfernaud@gmail.com>
2017-04-01 21:34:01 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+  var button = document.getElementById('fetch');
+  button.addEventListener('click', fetchArticle);
+};
+
+function fetchArticle() {
+  var http = 'https://'
+  var language = document.getElementById('language').value;
+  var apiUrl = '.wikipedia.org/api/rest_v1/page/mobile-sections/';
+  var title = document.getElementById('title').value;
+
+  var url = http + language + apiUrl + title;
+
+  var article;
+  var articleHTML;
+
+  var xmlhttp = new XMLHttpRequest(), json;
+
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+
+      json = JSON.parse(xmlhttp.responseText);
+      sections = json.lead.sections;
+      article = document.getElementById('article')
+      article.innerHTML = sections[0].text;
+
+    } else if (xmlhttp.status === 404) {
+      console.log('Page not found.');
+    }
+  };
+
+  xmlhttp.open('GET', url, true);
+  xmlhttp.send();
+};
