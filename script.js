d6347e5b7c92e7c2e0e1999bd84f7d2b8f25f6b5
lujanfernaud <lujanfernaud@gmail.com>
2017-04-02 23:14:41 +0100

    Add article title

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ function fetchArticle() {
   var json;
   var sections;
   var article;
+  var articleTitle;
   var page;
   var warningMessage;
 
@@ -38,18 +39,22 @@ function fetchArticle() {
 
   xmlhttp.onreadystatechange = function() {
     article = document.getElementById('article');
+    articleTitle = document.getElementById('article-title');
     page = document.getElementById('page-container');
     warningMessage = document.getElementById('warning-message');
 
     if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
 
       json = JSON.parse(xmlhttp.responseText);
+      title = json.lead.displaytitle;
       sections = json.lead.sections;
+      articleTitle.innerHTML = title;
       article.innerHTML = sections[0].text;
       warningMessage.className = 'warning-hidden';
 
     } else if (xmlhttp.status === 404 || xmlhttp.readyState === 4 && xmlhttp.status === 0) {
 
+      articleTitle.innerHTML = '';
       article.innerHTML = '';
       warningMessage.className = 'warning-visible';
     }
